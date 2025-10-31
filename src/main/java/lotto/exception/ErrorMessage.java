3fea960646a01c062697ace44b2135f788a6e9ba
Hheojiwon <0618amy@naver.com>
2025-11-01 00:53:53 +0900

    feat: 에러 메세지 작성

---
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -0,0 +1,24 @@
+package lotto.exception;
+
+public enum ErrorMessage {
+
+    INVALID_PURCHASE_AMOUNT_UNDER_MINIMUM("구입 금액은 1,000원 이상이어야 합니다."),
+    INVALID_PURCHASE_AMOUNT_NOT_DIVISIBLE("구입 금액은 1,000원 단위로 입력해야 합니다."),
+    INVALID_PURCHASE_AMOUNT_EMPTY("구입 금액이 비어 있습니다."),
+    INVALID_PURCHASE_AMOUNT_NOT_NUMBER("구입 금액은 숫자 형태로 입력해야 합니다."),
+
+    INVALID_LOTTO_NUMBER_NOT_NUMBER("로또 번호는 숫자 형태로 입력해야 합니다."),
+    INVALID_LOTTO_NUMBER_EMPTY("로또 번호가 비어 있습니다."),
+    INVALID_LOTTO_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_WINNING_NUMBER("당첨 번호에 중복된 숫자가 있습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/lotto/exception/LottoException.java
@@ -0,0 +1,8 @@
+package lotto.exception;
+
+public class LottoException extends IllegalArgumentException {
+
+    public LottoException(ErrorMessage errorMessage) {
+        super("[ERROR]" + errorMessage.getMessage());
+    }
+}
