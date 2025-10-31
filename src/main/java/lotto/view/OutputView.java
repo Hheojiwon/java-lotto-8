5c8f3a38652a790530a2de669100c94de6b6ae64
Hheojiwon <0618amy@naver.com>
2025-10-31 23:27:25 +0900

    feat: 금액, 당첨 번호 사용자 입력을 위한 출력 기능

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,16 @@
+package lotto.view;
+
+public class OutputView {
+
+    public void printLottoPriceInputMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printLottoWinNumbersInputMessage() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printLottoBonusNumbersInputMessage() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}
