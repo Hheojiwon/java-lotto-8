7b999d3bdbc778141886f8c121226a15a308a9f7
Hheojiwon <0618amy@naver.com>
2025-10-31 23:26:32 +0900

    feat: 사용자 입력 기능

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,14 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public String readInput() {
+        return Console.readLine();
+    }
+
+    public void close(){
+        Console.close();
+    }
+}
