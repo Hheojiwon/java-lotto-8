baec8ad3e2d89be67ab8ce7069613adcc5b474c1
Hheojiwon <0618amy@naver.com>
2025-11-03 23:09:39 +0900

    feat: 출력 요구 형식 맞춰서 당첨 통계를 출력하는 outputView 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
 
     public void printLottoPriceInputMessage() {
@@ -7,10 +9,35 @@ public class OutputView {
     }
 
     public void printLottoWinNumbersInputMessage() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
     }
 
-    public void printLottoBonusNumbersInputMessage() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+    public void printLottoBonusNumberInputMessage() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+    }
+
+    public void printPurchaseCount(int count) {
+        System.out.println("\n" + count + "개를 구매했습니다.");
+    }
+
+    public void printLottoNumbers(List<Integer> numbers) {
+        System.out.println(numbers);
+    }
+
+    public void printResultHeader() {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+    }
+
+    public void printRankResult(String description, int count) {
+        System.out.println(description + " - " + count + "개");
+    }
+
+    public void printProfitRate(double profitRate) {
+        System.out.println("총 수익률은 " + profitRate + "%입니다.");
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println(message);
     }
 }
