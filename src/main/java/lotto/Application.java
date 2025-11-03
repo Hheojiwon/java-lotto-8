9d1f92ef19bb6581965f6ffc64780ae44a2e2ae2
Hheojiwon <0618amy@naver.com>
2025-11-03 23:10:44 +0900

    feat: 로또 게임 시작하는 Application 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,32 @@
 package lotto;
 
+import lotto.util.LottoNumberParser;
+import lotto.util.RandomNumberGenerator;
+import lotto.util.RandomNumberGeneratorImpl;
+import lotto.validator.BonusNumberValidator;
+import lotto.validator.WinningNumberValidator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RandomNumberGenerator generator = new RandomNumberGeneratorImpl();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoNumberParser parser = new LottoNumberParser();
+        WinningNumberValidator winningNumberValidator = new WinningNumberValidator();
+        BonusNumberValidator bonusNumberValidator = new BonusNumberValidator();
+
+        LottoGame game = new LottoGame(
+                generator,
+                inputView,
+                outputView,
+                parser,
+                winningNumberValidator,
+                bonusNumberValidator
+        );
+
+        game.run();
     }
 }
